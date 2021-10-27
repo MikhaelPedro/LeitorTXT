be5fe2a5fd0b35dc664eae236ffeb411498f7d3c
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2021-10-26 23:40:02 -0300

    Atualizado

---
+++ 8. Atividade Spider 2/Scripts/Home.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    
+});
+
+$("#Btn_Enviar").click(function () {
+
+    var arquivo = new FormData();
+    arquivo.append($("#resultado")[0].files[0].name, $("#resultado")[0].files[0])
+    SendFile(arquivo);
+});
